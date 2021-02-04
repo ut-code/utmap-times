68d677e5513c64e926b0176739b8c5fe9f9a5eb8
kaing46 <78522605+kaing46@users.noreply.github.com>
2021-02-04 17:21:50 +0900

    aboutを追加中

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,34 @@ export default function IndexPage(
           </li>
         ))}
       </ul>
+      <div className="w-2/3">
+        <div className="h-48 pl-36 pt-28">
+          <h2 className="text-3xl left-14">About</h2>
+          <p className="text-yellow-500">UTmap とは</p>
+        </div>
+        <div className="h-32 pl-36">
+          <p className="text-3xl text-blue-800">
+            東大生のキャリア設計
+            <br />
+            プラットフォーム
+          </p>
+        </div>
+        <div className="h-56">
+          <p className="pl-36">
+            東大生が、より良い人生を切り開き、より良い未来をつくるために、東大生活を歩む「地図」を授けたい。
+            <br />
+            UTmap(ユー・ティー・マップ)は、東大生による東大生のためのキャリアプラットフォームとして、東大生のキャリアに大きな影響をもたらす「サークル」「進振り」「就活」という3つの意思決定をサポートします。
+            <br />
+            東大発ソーシャルスタートアップの「Pyxida(ピクシーダ)」が、総力を挙げて取材・編集したコンテンツをご活用ください。
+          </p>
+        </div>
+        <button
+          type="button"
+          className="h-10 w-30 ml-36 px-5 text-white bg-blue-800 hover:bg-gray-100"
+        >
+          もっと見る
+        </button>
+      </div>
     </Layout>
   );
 }
