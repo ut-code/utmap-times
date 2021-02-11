51816222d24d19c1c79b563ed8ef95eca65fc447
kaing46 <78522605+kaing46@users.noreply.github.com>
2021-02-11 13:34:24 +0900

    about2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { InferGetStaticPropsType } from "next";
 import Link from "next/link";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
+import Logo from "../components/Logo";
 import apolloClient from "../utils/apollo";
 import { IndexQuery } from "../__generated__/IndexQuery";
 
@@ -28,8 +29,8 @@ export default function IndexPage(
           </li>
         ))}
       </ul>
-      <div className="w-full flex">
-        <div className="w-2/3">
+      <div className="w-full flex relative">
+        <div className="w-2/3 z-10">
           <div className="px-40 pt-32 pb-28">
             <h2 className="text-5xl font-bold">About</h2>
             <p className="py-5 text-yellow-500">UTmap とは</p>
@@ -54,9 +55,33 @@ export default function IndexPage(
           </div>
         </div>
         <div className="w-1/3 relative">
-          <div className="h-2/3 w-full absolute bottom-0 py-60 bg-blue-900 text-center">
-            ロゴ
+          <Logo className="absolute bottom-0 left-0 w-full h-2/3 bg-gray-200 z-30" />
+        </div>
+        <div className="absolute top-0 right-0 w-3/4 h-full bg-purple-50" />
+      </div>
+      <div className="h-80 bg-pink-300" />
+      <div className="h-96 relative">
+        <div className="h-96 w-5/6 bg-purple-200" />
+        <div className="h-96 w-5/6 bg-gray-100 absolute right-0 top-72 z-10 py-20 flex">
+          <div className="h-56 w-2/5 pl-28 py-4 bg-red-300">
+            <h2 className="text-5xl font-bold">Circle</h2>
+            <p className="py-5 text-yellow-500">サークル選択のサポート</p>
+            <button
+              type="button"
+              className="h-12 w-64 text-white bg-blue-900 hover:bg-blue-500 mt-6"
+            >
+              もっと見る
+            </button>
           </div>
+          <p className="h-56 w-3/5 pl-24 pr-36 bg-yellow-300">
+            東大のサークルについてご紹介いたします。これはダミーテキストです。
+            ダミーテキストです。東大のサークルについてご紹介いたします。東大の
+            サークルについてご紹介いたします。これはダミーテキストです。
+            ダミーテキストです。東大のサークルについてご紹介いたします。東大の
+            サークルについてご紹介いたします。これはダミーテキストです。ダミー
+            テキストです。東大のサークルについてご紹介いたします。
+            東大のサークルについてご紹介いたします。これはダミーテキストです。
+          </p>
         </div>
       </div>
     </Layout>
