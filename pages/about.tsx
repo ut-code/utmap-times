df78c148444de143bb1b81efc2a69f8f54a8ce4d
inotani11 <78283981+inotani11@users.noreply.github.com>
2021-02-11 15:58:36 +0900

    What is UTmapです

---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+import apolloClient from "../utils/apollo";
+import { IndexQuery } from "../__generated__/IndexQuery";
+
+export default function AboutPage() {
+  return (
+    <Layout title="What is UTmap">
+      <Hero image="https://picsum.photos/800/600">
+        <h1 className="text-4xl p-32">What is UTmap</h1>
+      </Hero>
+      <p>こんにちは</p>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const queryResult = await apolloClient.query<IndexQuery>({
+    query: gql`
+      query IndexQuery {
+        allBanner {
+          title
+          image 
+          link
+            }
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: queryResult.data,
+  };
+}
