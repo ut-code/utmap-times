8190c6be3eaa4d981580df1a9ffd007b4aa6730e
chelproc <chelproc@outlook.com>
2021-02-25 07:37:53 +0000

    フッターデザイン修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,67 +1,52 @@
 import React from "react";
 import { FaTwitter, FaLine } from "react-icons/fa";
 import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
+import Link from "next/link";
+import clsx from "clsx";
 import Logo from "./Logo";
 
 export default function Footer() {
   return (
-    <footer className="py-8 h-400 bg-black">
-      <Logo white className="w-32 h-32 mx-auto" />
-      <div className="pt-7 text-center">
-        <button
-          type="button"
-          className="px-12 py-3 h-full text-white bg-gray-500 hover:bg-gray-300"
-        >
-          What is UTmap
-        </button>
+    <footer className="py-12 h-400 bg-black text-white">
+      <Logo white className="w-40 h-40 mx-auto" />
+      <div className="grid grid-cols-2 gap-2 w-60 mx-auto my-12">
+        {[
+          { title: "What is UTmap", linkTo: "/", wider: true },
+          { title: "Join us", linkTo: "/" },
+          { title: "Contact", linkTo: "/" },
+        ].map((buttonInfo) => (
+          <Link href={buttonInfo.linkTo}>
+            <a
+              className={clsx(
+                "block py-4 text-center bg-gray-800 hover:bg-gray-600",
+                { "col-span-2": buttonInfo.wider }
+              )}
+            >
+              {buttonInfo.title}
+            </a>
+          </Link>
+        ))}
       </div>
-      <div className="pt-5 text-center">
-        <span className="px-1">
-          <button
+      <div className="flex justify-center space-x-2 my-12">
+        {[
+          { Component: FaTwitter, linkTo: "https://example.com/" },
+          { Component: AiOutlineFacebook, linkTo: "https://example.com/2" },
+          { Component: AiOutlineInstagram, linkTo: "https://example.com/3" },
+          { Component: FaLine, linkTo: "https://example.com/4" },
+        ].map((snsInfo) => (
+          <a
+            key={snsInfo.linkTo}
             type="button"
-            className="h-full px-7 py-3 text-white bg-gray-500 hover:bg-gray-300"
+            className="block w-9 h-full hover:bg-blue-700 text-white"
+            href={snsInfo.linkTo}
           >
-            Join us
-          </button>
-        </span>
-        <span className="px-1">
-          <button
-            type="button"
-            className="h-full px-7 py-3 text-white bg-gray-500 hover:bg-gray-300"
-          >
-            Contact
-          </button>
-        </span>
+            <snsInfo.Component className="inline-block w-8 h-8" />
+          </a>
+        ))}
       </div>
-      <div className="pt-22 text-center">
-        <button
-          type="button"
-          className="w-9 h-full hover:bg-blue-700 text-white"
-        >
-          <FaTwitter className="inline-block w-4 h-4" />
-        </button>
-        <button
-          type="button"
-          className="w-9 h-full hover:bg-blue-700 text-white"
-        >
-          <AiOutlineFacebook className="inline-block w-4 h-4" />
-        </button>
-        <button
-          type="button"
-          className="w-9 h-full hover:bg-blue-700 text-white"
-        >
-          <AiOutlineInstagram className="inline-block w-4 h-4" />
-        </button>
-        <button
-          type="button"
-          className="w-9 h-full hover:bg-blue-700 text-white"
-        >
-          <FaLine className="inline-block w-4 h-4" />
-        </button>
-      </div>
-      <div className="p-14 pt-18 text-white text-center">
+      <div className="my-12 text-center">
         <p>Coded by ut.code();</p>
-        <p>Copyright © UTmap. All rights reserved.</p>
+        <p>Copyright &copy; UTmap. All rights reserved.</p>
       </div>
     </footer>
   );
