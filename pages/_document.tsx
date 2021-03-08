3ea13bcf89812535acfe7c1e2475529f420875b1
chelproc <chelproc@outlook.com>
2021-03-08 15:25:55 +0000

    styled-componentのSSR時にCSSが生成されない問題を修正

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+/**
+ * SSR時にstyled-componentsのスタイルを適用させるために必要
+ * https://styled-components.com/docs/advanced#nextjs
+ */
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
