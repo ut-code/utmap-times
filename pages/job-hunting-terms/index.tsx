64e1df29987f4a2ab53861677315cfce724bce30
KokiHayashi <1234.884.koki@gmail.com>
2022-02-27 11:58:02 +0900

    就活用語

--- pages/job-hunting-terms/index.tsx
+++ pages/job-hunting-terms/index.tsx
@@ -18,72 +18,140 @@ export default function JobHuntingTermIndexPage(
         title="DICTIONARY"
         subtitle="就活用語"
       />
-
-      <section className="bg-gray-200">
-        <div className="box-border md:box-content">
-          <header className="flex items-enter mb-2">
-            <h4 className="text-lg mr-2">戦略・フレームワーク説明</h4>
-            <div
-              aria-hidden="true"
-              className="flex-grow border-b border-gray-400"
-            />
-          </header>
-          <ul className="flex flex-wrap">
-            {props.jobHuntingTerms.map((term) => (
-              <div key={term.id} className="my-4">
-                {/* <header className="flex items-center mb-2"> */}
-                <button
-                  type="button"
-                  className="text-lg mr-2"
-                  onClick={() => {
-                    if (focusedJobHuntingTerm === term.id) {
-                      setFocusedJobHuntingTerm(null);
-                    } else {
-                      setFocusedJobHuntingTerm(term.id);
-                    }
-                  }}
-                >
-                  <div
-                    className={clsx(
-                      focusedJobHuntingTerm === term.id
-                        ? "inline-block mr-2 mb-2 bg-yellow-300"
-                        : "inline-block mr-2 mb-2"
-                    )}
+      <section className="bg-white flex flex-col">
+        <section className="bg-gray-200 m-3">
+          <div className="box-border md:box-content">
+            <header className="flex items-enter mb-2">
+              <h4 className="text-lg mr-2">戦略・フレームワーク説明</h4>
+              <div
+                aria-hidden="true"
+                className="flex-grow border-b border-gray-400"
+              />
+            </header>
+            <ul className="flex flex-wrap">
+              {props.jobHuntingTerms.map((term) => (
+                <div key={term.id} className="my-4">
+                  {/* <header className="flex items-center mb-2"> */}
+                  <button
+                    type="button"
+                    className="text-lg mr-2"
+                    onClick={() => {
+                      if (focusedJobHuntingTerm === term.id) {
+                        setFocusedJobHuntingTerm(null);
+                      } else {
+                        setFocusedJobHuntingTerm(term.id);
+                      }
+                    }}
                   >
-                    {term.name}
-                  </div>
+                    <div
+                      className={clsx(
+                        focusedJobHuntingTerm === term.id
+                          ? "inline-block mr-2 mb-2 bg-yellow-300"
+                          : "inline-block mr-2 mb-2"
+                      )}
+                    >
+                      {term.name}
+                    </div>
 
-                  <div
-                    aria-hidden
-                    className="flex-grow border-b border-gray-400"
-                  />
-                </button>
-                {/* </header> */}
+                    <div
+                      aria-hidden
+                      className="flex-grow border-b border-gray-400"
+                    />
+                  </button>
+                  {/* </header> */}
 
-                <ul>
-                  {focusedJobHuntingTerm !== null &&
-                    focusedJobHuntingTerm === term.id && (
-                      <li>
-                        説明
-                        <button
-                          type="button"
-                          onClick={() => {
-                            setFocusedJobHuntingTerm(null);
-                          }}
-                        >
-                          <div className={clsx("block py-1 px-2 text-sm")}>
-                            {props.jobHuntingTerms.find(
-                              (_term) => focusedJobHuntingTerm === _term.id
-                            )?.description ?? ""}
-                          </div>
-                        </button>
-                      </li>
-                    )}
-                </ul>
-              </div>
-            ))}
-          </ul>
-        </div>
+                  <ul>
+                    {focusedJobHuntingTerm !== null &&
+                      focusedJobHuntingTerm === term.id && (
+                        <li>
+                          説明
+                          <button
+                            type="button"
+                            onClick={() => {
+                              setFocusedJobHuntingTerm(null);
+                            }}
+                          >
+                            <div className={clsx("block py-1 px-2 text-sm")}>
+                              {props.jobHuntingTerms.find(
+                                (_term) => focusedJobHuntingTerm === _term.id
+                              )?.description ?? ""}
+                            </div>
+                          </button>
+                        </li>
+                      )}
+                  </ul>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <section className="bg-gray-200 m-3">
+          <div className="box-border md:box-content">
+            <header className="flex items-enter mb-2">
+              <h4 className="text-lg mr-2">戦略・フレームワーク説明2</h4>
+              <div
+                aria-hidden="true"
+                className="flex-grow border-b border-gray-400"
+              />
+            </header>
+            <ul className="flex flex-wrap">
+              {props.jobHuntingTerms.map((term) => (
+                <div key={term.id} className="my-4">
+                  {/* <header className="flex items-center mb-2"> */}
+                  <button
+                    type="button"
+                    className="text-lg mr-2"
+                    onClick={() => {
+                      if (focusedJobHuntingTerm === term.id) {
+                        setFocusedJobHuntingTerm(null);
+                      } else {
+                        setFocusedJobHuntingTerm(term.id);
+                      }
+                    }}
+                  >
+                    <div
+                      className={clsx(
+                        focusedJobHuntingTerm === term.id
+                          ? "inline-block mr-2 mb-2 bg-yellow-300"
+                          : "inline-block mr-2 mb-2"
+                      )}
+                    >
+                      {term.name}
+                    </div>
+
+                    <div
+                      aria-hidden
+                      className="flex-grow border-b border-gray-400"
+                    />
+                  </button>
+                  {/* </header> */}
+
+                  <ul>
+                    {focusedJobHuntingTerm !== null &&
+                      focusedJobHuntingTerm === term.id && (
+                        <li>
+                          説明
+                          <button
+                            type="button"
+                            onClick={() => {
+                              setFocusedJobHuntingTerm(null);
+                            }}
+                          >
+                            <div className={clsx("block py-1 px-2 text-sm")}>
+                              {props.jobHuntingTerms.find(
+                                (_term) => focusedJobHuntingTerm === _term.id
+                              )?.description ?? ""}
+                            </div>
+                          </button>
+                        </li>
+                      )}
+                  </ul>
+                </div>
+              ))}
+            </ul>
+          </div>
+        </section>
       </section>
     </Layout>
   );
