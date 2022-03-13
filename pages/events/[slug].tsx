332506e5b8282855b0515c95a20c706b98f0bd84
inotani11 <78283981+inotani11@users.noreply.github.com>
2022-03-13 10:28:50 +0900

    #196 LINEバーナーデザイン変更

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,5 +1,4 @@
 import { gql } from "@apollo/client";
-import Image from "next/image";
 import {
   GetStaticPathsResult,
   GetStaticPropsContext,
@@ -10,6 +9,7 @@ import dayjs from "dayjs";
 import ja from "dayjs/locale/ja";
 import ArticleContentContainer from "../../components/ArticleContentContainer";
 import Banners from "../../components/Banners";
+import LineBanners from "../../components/LineBanners";
 import Hero from "../../components/Hero";
 import Layout, { layoutSeoFragment } from "../../components/Layout";
 import RichTextRenderer from "../../components/RichTextRenderer";
@@ -243,30 +243,7 @@ export default function EventPage(
             />
           </div>
         )}
-        <div className="bg-gray-100 px-2 md:px-12 grid xs:grid-cols-2 md:grid md:grid-cols-4 xl:grid-cols-4">
-          <Image height={400} width={100} src="/images/line-2.png" />
-          <Image height={400} width={100} src="/images/line-1.png" />
-          <div className="col-span-2 pl-6 md:py-4">
-            <p className="text-secondary-main font-bold pt-8 text-2xl">
-              公式LINE登録で限定情報にアクセス
-            </p>
-            <div className="grid grid-cols-3 text-sm pb-4">
-              <div className="col-span-2 pt-8">
-                <p>UTmapでは、公式 LINEアカウントを通して</p>
-                <p>東大生向けの限定情報を配信しております。</p>
-                <p>時間割やクーポンなど、東大生の生活を彩る</p>
-                <p>様々な機能を提供しております</p>
-                <p>お見逃しなく!</p>
-              </div>
-              <Image height={1020} width={1800} src="/images/line-image.png" />
-            </div>
-            <Link href="https://lin.ee/oYnl12K">
-              <a className="inline-block w-full text-center px-12 py-3 m-4 text-sm font-bold bg-primary-main text-white">
-                公式LINE登録はこちら
-              </a>
-            </Link>
-          </div>
-        </div>
+        <LineBanners />
       </ArticleContentContainer>
       <div className="bg-primary-main">
         <div className="container mx-auto py-16 text-center">
